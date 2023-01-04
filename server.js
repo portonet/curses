92158e2fb672c61bf761bf2cc001b449245aea3a
Aline Porto Pereira <aline.porto-ext@hexagon.com>
2023-01-04 17:24:15 -0300

    criado o servideor api

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const express * require={express}
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.status(200).send({message: 'Olá mundo :D'});
+})
+
+app.listen(3001, ()=>{
+    console.log('Api rodando na porta 3001');
+} )
